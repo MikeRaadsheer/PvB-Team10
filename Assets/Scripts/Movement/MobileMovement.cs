6d03b6b0ff90a6525b2f852f7e068da019f1b4e7
MikeR <mikeraadsheer11@gmail.com>
2021-03-03 22:09:49 +0100

    - Imported TextMeshPro - Added Gyro Input

---
+++ Assets/Scripts/Movement/MobileMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class MobileMovement : MonoBehaviour
+{
+    private Gyroscope _gyro;
+    public TextMeshProUGUI gyroTxt;
+
+    void Start()
+    {
+        _gyro = Input.gyro;
+        _gyro.enabled = true;
+        gyroTxt.text= "testing";
+    }
+
+    void Update()
+    {
+        gyroTxt.text = 
+            "x: " + Math.Floor(_gyro.attitude.x * 100) + 
+            "\ny: " + Math.Floor(_gyro.attitude.y * 100) + 
+            "\nz: " + Math.Floor(_gyro.attitude.z * 100);
+    }
+}
