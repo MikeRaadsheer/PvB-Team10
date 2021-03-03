2b2a1b603d24ecc6c8017d3b8bddb10715778d35
JudaHensen <24311@ma-web.nl>
2021-03-03 15:35:12 +0000

    Added beginning of the point system.

---
+++ Assets/Scripts/PointSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public static class PointSystem
+{
+    private static int _points;
+
+    public static int Get()
+    {
+        if(_points == null)
+        {
+            _points = PlayerPrefs.GetInt("Points");
+            return _points;
+        }
+        return _points;
+    }
+
+    public static void Add(int amount)
+    {
+        _points += amount;
+    }
+
+    public static void Set(int value)
+    {
+        _points = value;
+        UpdateAmount(_points);
+    }
+
+
+    private static void UpdateAmount()
+    {
+        PlayerPrefs.SetInt("Points", _points);
+    }
+
+}
