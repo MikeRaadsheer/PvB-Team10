b53f40d7b013182b3727d5bac39486b90acd24b0
VolkenK <volkenkoelewijn@gmail.com>
2021-03-05 11:44:17 +0100

    Shop Slider and shop UI. pair-programmed with Juda.

---
+++ Assets/Scripts/ShopItemButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ShopItemButton : MonoBehaviour
+{
+    [SerializeField] private Shop.Hat _currentHat;
+    [SerializeField] private GameObject _shop;
+
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener( ()=> 
+            {
+                _shop.GetComponent<ShopPoints>().BuyItem(_currentHat);
+            }); 
+    }
+
+}
