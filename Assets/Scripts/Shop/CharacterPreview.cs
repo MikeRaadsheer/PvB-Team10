f7e03a248d063a01853f085dc7907922922132c6
JudaHensen <24311@ma-web.nl>
2021-03-08 16:19:22 +0100

    Implemented more art for the shop with Volken.

---
+++ Assets/Scripts/Shop/CharacterPreview.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shop {
+
+    public class CharacterPreview : MonoBehaviour
+    {
+
+        [SerializeField]
+        private float _rotationSpeed = 1f;
+        private float _rotation = 180f;
+
+        void Update()
+        {
+            //_rotation += _rotationSpeed * Time.deltaTime;
+            //_rotation %= 360;
+
+            GetComponent<Transform>().Rotate(0, _rotationSpeed * Time.deltaTime, 0, Space.Self);
+        }
+    }
+}
+
+
