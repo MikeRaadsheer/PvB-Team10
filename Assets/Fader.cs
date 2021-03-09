f981df489c0160c658ce89a88985e42b6398fdd7
sjorsDAwhoop <35691851+sjorsDAwhoop@users.noreply.github.com>
2021-03-09 14:17:29 +0100

    menu added

---
+++ Assets/Fader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fader : MonoBehaviour
+{
+    private CanvasGroup fader;
+    private float loadTime;
+    private float MinimumFadeTime = 3.0f;
+
+    private void Start()
+    {
+        fader = FindObjectOfType<CanvasGroup>();
+
+        fader.alpha = 1;
+
+        if (Time.time < MinimumFadeTime)
+            loadTime = MinimumFadeTime;
+        else
+            loadTime = Time.time;
+    }
+    private void Update()
+    {
+        if(Time.time < MinimumFadeTime)
+        {
+            fader.alpha = 1 - Time.time;
+        }
+
+        if(Time.time > MinimumFadeTime && loadTime != 0)
+        {
+            fader.alpha = Time.time - MinimumFadeTime;
+            if(fader.alpha >= 1)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+}
--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CanvasGroup fadegroup;
+    private float fadeInSpeed = 0.33f;
+
+    public RectTransform menuContainer;
+    public Transform PlayMenu;
+    public Transform ShopMenu;
+    public Transform CharacterMenu;
+
+    private Vector3 MenuPosition;
+
+    public int moveScreenX = 1280;
+    public int moveScreenY = 720;
+
+
+    private void Start()
     {
-        
+        fadegroup = FindObjectOfType<CanvasGroup>();
+
+        fadegroup.alpha = 1;
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        fadegroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+
+        menuContainer.anchoredPosition3D = Vector3.Lerp(menuContainer.anchoredPosition3D, MenuPosition, 0.1f);
     }
+
+    private void NavigateToMenu(int menuIndex)
+    {
+        switch (menuIndex)
+        {
+            // 0 = main menu
+            default:
+            case 0:
+                MenuPosition = Vector3.zero;
+                break;
+            //1 = shop
+            case 1:
+                MenuPosition = Vector3.right * moveScreenX;
+                break;
+            //2 = play
+            case 2:
+                MenuPosition = Vector3.left * moveScreenX;
+                break;
+            //3 = charactersettings
+            case 3:
+                MenuPosition = Vector3.down * moveScreenY;
+                break;
+        }
+    }
+
+    //buttons
+    public void OnCharactersClick()
+    {
+        NavigateToMenu(3);
+        Debug.Log("character clicked");
+    }
+    public void OnPlayClick()
+    {
+        NavigateToMenu(2);
+        Debug.Log("play clicked");
+
+    }
+    public void OnShopClick()
+    {
+        NavigateToMenu(1);
+        Debug.Log("shop clicked");
+    }
+    public void OnBackClick()
+    {
+        NavigateToMenu(0);
+        Debug.Log("go back");
+    }
+
 }
